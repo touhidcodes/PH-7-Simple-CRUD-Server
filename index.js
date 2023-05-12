466a5fc20800c996daf810c31d5a55dfdc264e68
Touhidur Zaman <touhidcodes@gmail.com>
2023-05-12 16:26:48 +0600

    post-data-to-mongodb

--- index.js
+++ index.js
@@ -27,11 +27,18 @@ async function run() {
 	try {
 		// Connect the client to the server	(optional starting in v4.7)
 		await client.connect();
+		const database = client.db("usersDB");
+		const usersCollection = database.collection("users");
+
+		app.post("/users", async (req, res) => {
+			const user = req.body;
+			console.log("new user", user);
+			const result = await usersCollection.insertOne(user);
+			res.send(result);
+		});
+
 		// Send a ping to confirm a successful connection
-		await client.db("admin").command({ ping: 1 });
-		console.log(
-			"Pinged your deployment. You successfully connected to MongoDB!"
-		);
+		
 	} finally {
 		// Ensures that the client will close when you finish/error
 		// await client.close();
