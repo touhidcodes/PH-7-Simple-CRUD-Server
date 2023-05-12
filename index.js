08d05e60e1ea7c58c77f4ad7fcb6773884dbf11f
Touhidur Zaman <touhidcodes@gmail.com>
2023-05-12 12:30:39 +0600

    connect-to-mongodb

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// touhidcodes
+// nBSplzuYHiyDNW5t
+
+const uri =
+	"mongodb+srv://touhidcodes:nBSplzuYHiyDNW5t@cluster0.57whvd4.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	},
+});
+
+async function run() {
+	try {
+		// Connect the client to the server	(optional starting in v4.7)
+		await client.connect();
+		// Send a ping to confirm a successful connection
+		await client.db("admin").command({ ping: 1 });
+		console.log(
+			"Pinged your deployment. You successfully connected to MongoDB!"
+		);
+	} finally {
+		// Ensures that the client will close when you finish/error
+		// await client.close();
+	}
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+	res.send("CRUD server is running....");
+});
+
+app.listen(port, () => {
+	console.log(`Simple CRUD app listening on port ${port}`);
+});
