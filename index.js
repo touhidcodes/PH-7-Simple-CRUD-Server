6de4fe60e7feeff1cfee8502f3f5d381599c345d
Touhidur Zaman <touhidcodes@gmail.com>
2023-05-12 22:44:46 +0600

    each-user-server-data

--- index.js
+++ index.js
@@ -38,6 +38,13 @@ async function run() {
 			res.send(result);
 		});
 
+		app.get("/user/:id", async (req, res) => {
+			const id = req.params.id;
+			const query = { _id: new ObjectId(id) };
+			const result = await usersCollection.findOne(query);
+			res.send(result);
+		});
+
 		app.post("/users", async (req, res) => {
 			const user = req.body;
 			console.log("new user", user);
